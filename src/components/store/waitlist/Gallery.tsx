a3369046302526287681581cc7f144eff850ea8a
Gideon Olayode <99191347+SuezDevelopment@users.noreply.github.com>
2023-04-01 22:05:31 +0000

    call waitlist modal and gallery marquee

--- src/components/store/waitlist/Gallery.tsx
+++ src/components/store/waitlist/Gallery.tsx
@@ -13,17 +13,19 @@ export default function Gallery() {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
                     <div className="col-span-1 md:col-span-2 lg:col-span-3">
                         <div className="flex flex-col items-center">
-                            <img
-                                src="https://images.unsplash.com/photo-1504349147817-111111111111?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                                alt="A picture of a cat"
-                            />
-                            <div className="flex flex-col items-center">
-                                <h3 className="text-xl font-bold text-center mb-2">
-                                    A Cat
-                                </h3>
-                                <p className="text-sm text-gray-500">
-                                    A cat is a type of animal.
-                                </p>
+                            <div className="py-12 animate-marquee whitespace-nowrap">
+                                <span className="mx-4 text-4xl">Marquee Item 1</span>
+                                <span className="mx-4 text-4xl">Marquee Item 2</span>
+                                <span className="mx-4 text-4xl">Marquee Item 3</span>
+                                <span className="mx-4 text-4xl">Marquee Item 4</span>
+                                <span className="mx-4 text-4xl">Marquee Item 5</span>
+                            </div>
+                            <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+                                <span className="mx-4 text-4xl">Marquee Item 11</span>
+                                <span className="mx-4 text-4xl">Marquee Item 12</span>
+                                <span className="mx-4 text-4xl">Marquee Item 13</span>
+                                <span className="mx-4 text-4xl">Marquee Item 14</span>
+                                <span className="mx-4 text-4xl">Marquee Item 15</span>
                             </div>
                         </div>
                     </div>
--- src/components/store/waitlist/HeroButton.tsx
+++ src/components/store/waitlist/HeroButton.tsx
@@ -41,7 +41,7 @@ export default function HeroButton(){
 			})();
 
 			setTimeout(() => {
-				<WaitlistModal />
+				WaitlistModal()
 			}, 200);
 		}
 	}, []);
