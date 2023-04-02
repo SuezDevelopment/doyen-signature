cdbb4abdfb53dfc798a71039a50ce1b692d1693d
Gideon Olayode <99191347+SuezDevelopment@users.noreply.github.com>
2023-04-02 02:16:41 +0000

    getAllSignaturesByDoyenPost

---
+++ src/utils/getuserposts.ts
@@ -0,0 +1,26 @@
+import * as IGAPI from './'
+import { writeFileSync } from 'fs'
+import { IContext } from "./IContext"
+
+const target = "signaturesbydoyen"
+
+async function getContext(target:string): Promise<IContext> {
+    const ctxFilename = `../ctx/${target}.json`
+    let ctx = null
+    try {
+        // Tries to read the context from ctx path
+        return require(ctxFilename)
+    } catch (e) {
+        // If not found, creating a new ctx
+        ctx = await IGAPI.auth(target)
+    }
+    writeFileSync(ctxFilename, JSON.stringify(ctx), { encoding: "utf-8", flag: "w+" })
+    return ctx
+}
+
+export async function getAllSignaturesByDoyenPost(){
+    await getContext(target).then(async(ctx) =>{
+        const user = await IGAPI.getUser(target, ctx)
+        return await IGAPI.getAllUserPosts(user, ctx, {first: 12, after: null})
+    }).catch(e => console.log(e))
+}
